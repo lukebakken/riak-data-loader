37dcddb09b588bb0f361f65a72323696dd1a7410
Luke Bakken <luke@bowbak.org>
2014-01-12 11:22:36 -0800

    Updated for 2.0.0pre10

--- tweets-idx-setup.sh
+++ tweets-idx-setup.sh
@@ -9,8 +9,11 @@ if [[ ! -s twitter-schema.xml ]]
 then
   echo 'Required file twitter-schema.xml not found, exiting!'
 fi
-curl -XPUT -iH 'Content-Type: application/xml' 'http://localhost:8098/yz/schema/tweets-sch' --data-binary @twitter-schema.xml
+curl -XPUT -iH 'Content-Type: application/xml' 'http://localhost:8098/search/schema/tweets-sch' --data-binary @twitter-schema.xml
 
-curl -XPUT -iH 'Content-Type: application/json' http://localhost:8098/yz/index/tweets-idx -d '{"schema":"tweets-sch"}'
+curl -XPUT -iH 'Content-Type: application/json' http://localhost:8098/search/index/tweets-idx -d '{"schema":"tweets-sch"}'
 
-curl -XPUT -iH 'Content-Type: application/json' 'http://localhost:8098/buckets/tweets/props' -d '{"props":{"yz_index":"tweets-idx"}}'
+riak-admin bucket-type create tweets-type '{"props":{}}'
+riak-admin bucket-type activate tweets-type
+
+curl -iH 'Content-Type: application/json' -XPUT 'http://localhost:8098/types/tweets-type/props' --data-binary @tweets-bucket-type.json
