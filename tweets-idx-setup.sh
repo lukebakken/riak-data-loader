495ab5c6e30a6e92505dba2a8cfd3f519adbe621
Luke Bakken <luke@bowbak.org>
2014-07-16 17:35:02 +0000

    2.0.0rc1 testing tweaks

--- tweets-idx-setup.sh
+++ tweets-idx-setup.sh
@@ -12,8 +12,12 @@ then
 fi
 
 curl -XPUT -iH 'Content-Type: application/xml' 'http://localhost:8098/search/schema/tweets-sch' --data-binary @twitter-schema.xml
+sleep 5
 
 curl -XPUT -iH 'Content-Type: application/json' http://localhost:8098/search/index/tweets-idx -d '{"schema":"tweets-sch"}'
+sleep 10
 
 riak-admin bucket-type create tweets-type '{"props":{"search_index":"tweets-idx"}}'
+sleep 5
+
 riak-admin bucket-type activate tweets-type
